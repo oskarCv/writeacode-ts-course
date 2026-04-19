affd61723ec5cf3c43a738017f786535c1c2aa38
oskarCv <oskar.cv91@gmail.com>
2026-04-19 17:26:37 -0600

    Adding numbers to typings

---
+++ src/04_numbers.ts
@@ -0,0 +1,10 @@
+(() => {
+  let price = 19.99; // TypeScript infers that price is of type number.
+  console.log(price);
+  let productName: string = "Book"; // Explicitly annotating the type as string
+  console.log(productName);
+  let tax: 0.3; // Declaring a boolean variable without initializing it type should be included due to there is no way to inferr it.
+  console.log(tax); // This will log undefined because tax is declared but not initialized.
+  const stock: number = 100; // A constant variable of type number, not Number (the object wrapper type).
+  console.log("Stock: ", stock);
+})();
